5ea87410cba3273543ff5908383057ca87c80030
raeven <raeven@yandex.com>
2017-09-14 22:19:26 +0000

    add config file

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
 
+func init() {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.AddConfigPath(".")
+}
 func main() {
 	fmt.Println("Hi there")
 }
