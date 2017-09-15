2a74bd82fc913d1025fbf993552cd0f8c1932568
raeven <raeven@yandex.com>
2017-09-15 00:34:14 +0000

    add user functions

--- components/calendar/calendar.go
+++
@@ -1,5 +0,0 @@
-package calendar
-
-import (
-)
-
--- components/user/user.go
+++ components/user/user.go
@@ -1,5 +1,10 @@
 package user
 
+import (
+	"github.com/PuloV/ics-golang"
+	"time"
+)
+
 var usersArray []User
 
 type User struct {
@@ -7,6 +12,21 @@ type User struct {
 	JiraUsername string
 	IcsLink      string
 }
-func Initialize(users []User){
+
+func Initialize(users []User) {
 	usersArray = users
+}
+func Get() []User {
+	return usersArray
+}
+func (u *User) Events(t time.Time) ([]*ics.Event, error) {
+	parser := ics.New()
+	inputChan := parser.GetInputChan()
+	inputChan <- u.IcsLink
+	parser.Wait()
+	cal, err := parser.GetCalendars()
+	if err != nil {
+		return nil, err
+	}
+	return cal[0].GetEventsByDate(t)
 }
